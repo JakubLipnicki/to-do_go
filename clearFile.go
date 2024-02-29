1480bc321a206d85ad889b6ff7273a73cb2bd872
JakubLipnicki <jakublipnicki1@gmail.com>
2024-02-29 18:43:01 +0100

    deleting all tasks functionality

--- clearFile.go
+++ clearFile.go
@@ -1,8 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"os"
+)
 
 func clearFile() error {
-	fmt.Println("Clear test")
+	err := os.Remove("tasklist.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	file, err := os.Create("tasklist.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
 	return nil
 }
